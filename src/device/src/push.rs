98115e5ce57ae06c7abd148ab9a88df79d5faabe
Heráclito <heraclitoqsaldanha@gmail.com>
2022-02-08 16:29:05 -0300

    refactoring the update and push function

--- src/device/src/push.rs
+++ src/device/src/push.rs
@@ -4,15 +4,13 @@ use std::fs;
 impl Device {
 	pub fn push(&self) -> Result<(), DeviceError> {
 		{
-			let mut path_pwm_state = self.path.clone();
-			path_pwm_state.push("pwm1_enable");
+			let path_pwm_state = self.path.join("pwm1_enable");
 			if let Err(_) = fs::write(&path_pwm_state, self.pwm_state.to_string()){
 				return Err(DeviceError::WritePwmValue{path: path_pwm_state});
 			}
 		}
 		{
-			let mut path_pwm_value = self.path.clone();
-			path_pwm_value.push("pwm1");
+			let path_pwm_value = self.path.join("pwm1");
 			if let Err(_) = fs::write(&path_pwm_value, self.pwm_value.to_string()){
 				return Err(DeviceError::WritePwmValue {path: path_pwm_value});
 			}
--- src/device/src/update.rs
+++ src/device/src/update.rs
@@ -7,61 +7,40 @@ impl Device{
 			return Err(DeviceError::NotExits {path: self.path.clone()});
 		}
 		self.name = {
-			let mut path_name = self.path.clone();
-			path_name.push("name");
+			let path_name = self.path.join("name");
 			match fs::read_to_string(&path_name) {
 				Ok(name) => name.trim_matches('\n').to_string(),
 				Err(_) => return Err(DeviceError::ReadName{path: path_name})
 			}
 		};
 		self.pwm_value = {
-			let mut path_pwm_value = self.path.clone();
-			path_pwm_value.push("pwm1");
+			let path_pwm_value = self.path.join("pwm1");
 			match fs::read_to_string(&path_pwm_value){
 				Ok(string_pwm_value) => match string_pwm_value.trim_matches('\n').parse::<i32>(){
 					Ok(value) => value,
-					Err(_) => {
-						return Err(DeviceError::ReadPwmValue{path: path_pwm_value})
-					}
+					Err(_) => return Err(DeviceError::ReadPwmValue{path: path_pwm_value})
 				},
-				Err(_) => {
-					return Err(DeviceError::ReadPwmValue{path: path_pwm_value})
-				}
+				Err(_) => return Err(DeviceError::ReadPwmValue{path: path_pwm_value})
 			}
 		};
-		self.pwm_max = {
-			let mut path_pwm_max = self.path.clone();
-			path_pwm_max.push("pwm1_max");
-			match fs::read_to_string(&path_pwm_max){
-				Ok(string_pwm_max) => {string_pwm_max.trim_matches('\n').parse::<i32>().unwrap_or(255)}
-				Err(_) => {255}
-			}
-		};
+		self.pwm_max = fs::read_to_string(self.path.join("pwm1_max")).unwrap_or(255.to_string()).trim_matches('\n').parse::<i32>().unwrap_or(255);
 		self.pwm_state = {
-			let mut path_pwm_state = self.path.clone();
-			path_pwm_state.push("pwm1_enable");
-			match fs::read_to_string(&path_pwm_state) {
+			let path_pwm_state = self.path.join("pwm1_enable");
+			match fs::read_to_string(&path_pwm_state){
 				Ok(string_pwm_state) => PwmState::from_string(string_pwm_state.trim_matches('\n').to_string()),
-				Err(_) => {
-					return Err(DeviceError::ReadPwmState{path: path_pwm_state})
-				}
+				Err(_) => return Err(DeviceError::ReadPwmState{path: path_pwm_state})
 			}
 		};
 		self.temp_value = {
-			let mut path_temp_value = self.path.clone();
-			path_temp_value.push("temp1_input");
+			let path_temp_value = self.path.join("temp1_input");
 			match fs::read_to_string(&path_temp_value){
 				Ok(string_temp_value) => {
 					match string_temp_value.trim_matches('\n').parse::<i32>(){
 						Ok(value) => value,
-						Err(_) => {
-							return Err(DeviceError::ReadTempValue{path: path_temp_value})
-						}
+						Err(_) => return Err(DeviceError::ReadTempValue{path: path_temp_value})
 					}
 				}
-				Err(_) => {
-					return Err(DeviceError::ReadTempValue{path: path_temp_value})
-				}
+				Err(_) => return Err(DeviceError::ReadTempValue{path: path_temp_value})
 			}
 		};
 		Ok(self)
