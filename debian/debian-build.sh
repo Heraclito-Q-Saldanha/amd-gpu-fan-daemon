66afb7f3aee617f3a17d04458d8e0d220e31a448
Heráclito <heraclitoqsaldanha@gmail.com>
2022-02-10 00:32:19 -0300

    refactoring script Debian package

--- debian/debian-build.sh
+++ debian/debian-build.sh
@@ -1,37 +1,62 @@
 #/bin/sh
 
-mkdir -p ./pkgroot/{etc/systemd/system,bin}
-
-if (( $# > 0 ))
-then
-	if [ $1 == "--use-musl" ];
-	then
-		RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --target=$(arch)-unknown-linux-musl --manifest-path=../Cargo.toml
-		mv ../target/$(arch)-unknown-linux-musl/release/amd-gpu-fan-daemon ./pkgroot/bin
-	fi
-else
-	RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --target=$(arch)-unknown-linux-gnu --manifest-path=../Cargo.toml
-	mv ../target/$(arch)-unknown-linux-gnu/release/amd-gpu-fan-daemon ./pkgroot/bin
-fi
-
-strip ./pkgroot/bin/amd-gpu-fan-daemon
-
-cp ../amd-gpu-fan.service ./pkgroot/etc/systemd/system
-cp -r ./DEBIAN ./pkgroot
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
 pkgversion=$(cat ../Cargo.toml | grep version | grep -o '".*"' | sed 's/"//g' | sed 's/[^0-9.]*//g')
 pkgname=$(cat ../Cargo.toml | grep name | grep -o '".*"' | sed 's/"//g')
 pkgdescription=$(cat ../Cargo.toml | grep description | grep -o '".*"' | sed 's/"//g')
 pkgarchitecture=$(dpkg-architecture -q DEB_HOST_ARCH)
-pkgsize=$(du -s ./pkgroot | cut -f1)
+pkgsize=0
 pkgfile=$(echo "./"${pkgname}"_"${pkgarchitecture}"_V"${pkgversion}".deb")
 
-echo "Installed-Size: "$pkgsize >> ./pkgroot/DEBIAN/control
-echo "Description: "$pkgdescription >> ./pkgroot/DEBIAN/control
-echo "Version: "$pkgversion >> ./pkgroot/DEBIAN/control
-echo "Architecture: "$pkgarchitecture >> ./pkgroot/DEBIAN/control
 
-dpkg-deb -b ./pkgroot ${pkgfile}
-sha512sum ${pkgfile} >> ${pkgfile}.sha512
+compile(){
+	RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --target=$(arch)-unknown-linux-gnu --manifest-path=../Cargo.toml
+	mv ../target/$(arch)-unknown-linux-gnu/release/amd-gpu-fan-daemon ./pkgroot/bin
+	strip ./pkgroot/bin/amd-gpu-fan-daemon
+	pkgsize=$(du -s ./pkgroot | cut -f1)
+}
 
+compile_musl(){
+	RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --target=$(arch)-unknown-linux-musl --manifest-path=../Cargo.toml
+	mv ../target/$(arch)-unknown-linux-musl/release/amd-gpu-fan-daemon ./pkgroot/bin
+	strip ./pkgroot/bin/amd-gpu-fan-daemon
+	pkgsize=$(du -s ./pkgroot | cut -f1)
+}
+
+config_files(){
+	mkdir -p ./pkgroot/{etc/systemd/system,bin}
+	cp ../amd-gpu-fan.service ./pkgroot/etc/systemd/system
+	cp -r ./DEBIAN ./pkgroot
+}
+
+config_control(){
+	echo "Installed-Size: "$pkgsize >> ./pkgroot/DEBIAN/control
+	echo "Description: "$pkgdescription >> ./pkgroot/DEBIAN/control
+	echo "Version: "$pkgversion >> ./pkgroot/DEBIAN/control
+	echo "Architecture: "$pkgarchitecture >> ./pkgroot/DEBIAN/control
+}
+
+pack(){
+	dpkg-deb -b ./pkgroot ${pkgfile}
+	sha512sum ${pkgfile} > ${pkgfile}.sha512
+}
+
+
+config_files
+if (( $# > 0 ))
+then
+	if [ $1 == "--use-musl" ];
+	then
+		compile_musl
+	else
+		compile
+	fi
+else
+	compile
+fi
+config_control
+pack
 rm -rf ./pkgroot
+
+
